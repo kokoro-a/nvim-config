a7e01e6a7db6d7ef75e087b32ed2c5a2d76cb592
kokoro-a <41562818+kokoro-a@users.noreply.github.com>
2025-07-24 11:36:27 +0000

    keymap: pm ProjectMgr

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,5 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("t", "jk", "<C-\\><C-n>")
+map("n", "<leader>pm", "<cmd> ProjectMgr <cr>", { desc = "Project Manager" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
