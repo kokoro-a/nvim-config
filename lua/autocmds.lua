b3ad24614582d25a0cc701b1f32ddc7dbc022912
kokoro-a <41562818+kokoro-a@users.noreply.github.com>
2025-07-24 12:07:32 +0000

    NvimTreeOpen when VimEnter

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,3 +1,12 @@
 require "nvchad.autocmds"
 
 vim.cmd("cabbrev pm ProjectMgr")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("lazy").load({ plugins = { "nvim-tree.lua" } })
+      vim.cmd("NvimTreeOpen")
+    end
+  end,
+})
