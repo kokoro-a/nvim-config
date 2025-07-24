e61f04a12e866dbd485a8fde25a1e15f2318b468
kokoro-a <41562818+kokoro-a@users.noreply.github.com>
2025-07-24 23:32:29 +0000

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,7 +12,6 @@ return {
     end,
   },
 
-  -- test new blink
   { import = "nvchad.blink.lazyspec" },
 
   {
@@ -34,32 +32,5 @@ return {
   {
     'github/copilot.vim',
     lazy = false,
-  },
-
-  -- markdown preview
-  {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    keys = {
-      {
-        "<leader>op",
-          function()
-          local peek = require("peek")
-              if peek.is_open() then
-          peek.close()
-          else
-          peek.open()
-          end
-        end,
-        desc = "Peek (Markdown Preview)",
-      },
-    },
-
-    config = function()
-        require("peek").setup()
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
   },
 }
